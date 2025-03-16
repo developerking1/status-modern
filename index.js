41c14989ebb54bd5c9ea7bc1f99aa5bfc32548c9
developer_king <renafernandesjorge11@gmail.com>
2025-03-16 08:35:06 +0000

    Atualizar o index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🧶 | /ajuda • sam-bot.xyz", "🎶 | Me adicione no seu servidor ", "📆 | Resgate o seu daily", "👤 | Estou a divertir e ajudar +70k usuários", "🫐 | Você é uma pessoa incrível", "✨️| Servidor de Suporte no Perfil", "👾 | Em 703 servidores"];
+const statusMessages = ["🧶 | /ajuda • sam-bot.xyz", "🎶 | Me adicione no seu servidor ", "📆 | Resgate o seu daily", "👤 | Estou a divertir e ajudar +70k usuários", "🫐 | Você é uma pessoa incrível", "✨️| Servidor de Suporte no Perfil", "👾 | Em 705 servidores"];
 const statusTypes = [ 'online', 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
